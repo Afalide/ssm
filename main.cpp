de694732e849f081a2451ff6af55855e16ffce0b
Afalide <afalide@hotmail.fr>
2017-07-27 22:06:08 +0200

    fix stuff and cleanup

--- main.cpp
+++ main.cpp
@@ -4,14 +4,23 @@
 #include <type_traits>
 #include <limits>
 
+////////////////////////////////////////////////////////////////////////////////
+
+// Forward declarations
+
 struct i_state_dtor;
 struct i_slot;
 
+template <typename t_state, typename...t_states>
+struct create_state_impl;
+
+////////////////////////////////////////////////////////////////////////////////
+
 struct i_state
 {
+    i_slot* m_owner;
 	i_state_dtor* m_dtor;
 
-    i_slot* m_owner;
     i_state()
         : m_owner (nullptr)
 		, m_dtor (nullptr)
@@ -20,6 +29,8 @@ struct i_state
     i_state(const i_state& other) = delete;
     i_state& operator=(const i_state& other) = delete;
 };
+
+////////////////////////////////////////////////////////////////////////////////
 
 struct i_slot
 {
@@ -42,11 +53,9 @@ struct i_slot
     {
         // Delete the old state
 
-		//delete_state_impl<t_old_state>::impl(idx, this);
-
 		t_old_state* old_state = dynamic_cast<t_old_state*>(m_states[idx]);
 		assert(nullptr != old_state);
-		old_state->m_dtor->perform_delete(old_state);
+		old_state->m_dtor->perform_delete();
 
         // Replace it with the new one
 
@@ -54,7 +63,10 @@ struct i_slot
     }
 };
 
+////////////////////////////////////////////////////////////////////////////////
+
 // SFINAE functions to create and delete a state's children
+// if and only if the given state has children
 
 template <typename _t_state>
 void sfinae_create_all_slots_of(typename std::enable_if<std::is_base_of<i_slot, _t_state>::value, _t_state>::type* st)
@@ -78,36 +90,11 @@ inline void sfinae_delete_all_slots_of(typename std::enable_if<!std::is_base_of<
 {
 }
 
-// Variadic functions to create all the children (slots) of a state
+////////////////////////////////////////////////////////////////////////////////
 
-//template <unsigned int idx, typename t_state>
-//void create_slot_impl(i_slot* parent_slot)
-//{
-////    std::cout << __PRETTY_FUNCTION__<< " - creating state at slot " << idx <<  std::endl;
-//
-//    // Create the new state at the specified slot index
-//
-//    t_state* new_state = new t_state;
-//    new_state->m_slot_pos = idx;
-//    new_state->m_owner = parent_slot;
-//    parent_slot->m_states[idx] = new_state;
-//
-//    // If the new state has children, recursively create them
-//
-//    sfinae_create_all_slots_of<t_state>(new_state);
-//}
-//
-//template <unsigned int idx, typename t_state, typename...t_states>
-//void create_all_slots_extract(i_slot* parent_slot)
-//{
-//    create_slot_impl<idx, t_state>(parent_slot);
-//    create_all_slots_extract<idx + 1, t_states...>(parent_slot);
-//}
-//
-//template <unsigned int idx>
-//void create_all_slots_extract(i_slot*)
-//{
-//}
+// MPL function to create a single state or a list of states
+// This will invoke the SFINAE function above to determine if each of the
+// create state also requires to instantiate it's children
 
 // Routing works as follows:
 //   impl <A, {B,C,D}>    // user writes this (which calls a router)
@@ -159,94 +146,59 @@ struct create_state_impl<t_state>
     }
 };
 
-// Variadic functions to delete all the children of a state
+////////////////////////////////////////////////////////////////////////////////
 
-//template <unsigned int idx, typename t_state>
-//void delete_slot_impl(i_slot* parent_slot)
-//{
-//    t_state* st = static_cast<t_state*> (parent_slot->m_states[idx]);
-//
-//    // If the state has slots, delete them
-//
-//    sfinae_delete_all_slots_of<t_state>(st);
-//
-//    // Delete the state
-//
-//    delete st;
-//    parent_slot->m_states[idx] = nullptr;
-//}
-//
-//template <unsigned int idx, typename t_state, typename...t_states>
-//void delete_all_slots_extract(i_slot* parent_slot)
-//{
-//    delete_slot_impl<idx, t_state>(parent_slot);
-//    delete_all_slots_extract<idx + 1, t_states...>(parent_slot);
-//}
-//
-//template <unsigned int idx>
-//void delete_all_slots_extract(i_slot*)
-//{
-//}
-
-template <typename t_state, typename...t_states>
-struct delete_state_impl
-{
-    static void impl(unsigned int idx, i_slot* owner)
-    {
-        //std::cout << "router calls impl" << std::endl;
-        delete_state_impl<t_state>::impl(idx, owner);
-        //std::cout << "router calls next (maybe router or direct impl)" << std::endl;
-        delete_state_impl<t_states...>::impl(idx + 1, owner);
-    }
-};
-
-// Specialization, which acts as the implementation
-
-template <typename t_state>
-struct delete_state_impl<t_state>
-{
-    static void impl(unsigned int idx, i_slot* owner)
-    {
-        t_state* st = static_cast<t_state*> (owner->m_states[idx]);
-
-        // If the state has slots, delete them
-
-        sfinae_delete_all_slots_of<t_state>(st);
-
-        // Delete the state
-
-        delete st;
-        owner->m_states[idx] = nullptr;
-    }
-};
-
-// Provides an destructor for a specific state
-// That implementation can be passed as a pointer 
-// to deffer it's execution
+// Deleting a state uses a different procedure because of transitions.
+// When deleting a state, one of it's child may have changed. Using an i_state
+// reference is not sufficient, because each child state may also have children,
+// therefore the SFINAE procedure for children deletion needs to be invoked on
+// the state to delete, which requires the true derived type of said state to be known.
+// This method uses a functor which can be carried around in an opaque type.
+// Invoking this functor will delete the given state and invalidate the functor
+// object (delete *this).
 
 struct i_state_dtor
 {
 	virtual ~i_state_dtor(){}
-	virtual void perform_delete(i_state*) = 0;
+	virtual void perform_delete() = 0;
 };
 
 template <typename t_state>
 struct state_dtor : public i_state_dtor
 {
-	virtual void perform_delete(i_state* st) override
+    t_state* m_state_to_delete;
+
+    state_dtor(t_state* st)
+        : m_state_to_delete(st)
+    { }
+
+    virtual ~state_dtor( )
+    {
+        m_state_to_delete = nullptr;
+    }
+
+    state_dtor(const state_dtor&) = delete;
+    state_dtor& operator=(const state_dtor&) = delete;
+
+	virtual void perform_delete() override
 	{
-		t_state* true_type_state = dynamic_cast<t_state*>(st);
-		assert(nullptr != true_type_state);
+        // Be safe, check first
 
-		//t_state* true_type_state = static_cast<t_state*>(st);  // Cheaper, use this form for release
+		//t_state* true_type_state = dynamic_cast<t_state*>(m_state_to_delete);
+		//assert(nullptr != true_type_state);
+        assert(nullptr != m_state_to_delete);
 
-		// Use the SFINAE form to determine if the child states must also be deleted
-		sfinae_delete_all_slots_of<t_state>(true_type_state);
+		//t_state* true_type_state = static_cast<t_state*>(st);  // Cheaper, use this instead of
+                                                                 // dynamic casts in release config
+
+		// Delete the state's children if required
+
+		sfinae_delete_all_slots_of<t_state>(m_state_to_delete);
 
 		// Keep the owner's infos
 
-		i_slot* owner = st->m_owner;
-		unsigned int idx = true_type_state->m_slot_pos;
+		i_slot* owner = m_state_to_delete->m_owner;
+		unsigned int idx = m_state_to_delete->m_slot_pos;
 
 		// Update the owner
 
@@ -254,9 +206,9 @@ struct state_dtor : public i_state_dtor
 
 		// Delete the state
 
-		delete st;
+		delete m_state_to_delete;
 
-		// Delete ourselves, the state object won't do it for us
+		// Delete ourselves
 
 		delete this;
 	}
@@ -279,9 +231,7 @@ struct slot
 
     virtual void create_all_slots() override
     {
-//        std::cout << __PRETTY_FUNCTION__<< std::endl;
         m_states = new i_state* [sizeof...(t_states)];
-//        create_all_slots_extract<t_states...>(0, this);
         create_state_impl<t_states...>::impl(0, this);
     }
 
@@ -293,7 +243,7 @@ struct slot
 
 		for (unsigned int i = 0; i < sizeof...(t_states); ++i)
 		{
-			m_states[i]->m_dtor->perform_delete(m_states[i]);
+			m_states[i]->m_dtor->perform_delete();
 			m_states[i] = nullptr;
 		}
 
@@ -312,7 +262,7 @@ struct state : public i_state
     state()
         : m_slot_pos(std::numeric_limits<unsigned int>::max())
     {
-		m_dtor = new state_dtor<t_crt>;
+		m_dtor = new state_dtor<t_crt>(static_cast<t_crt*>(this));
     }
 
     virtual ~state()
@@ -325,7 +275,7 @@ struct state : public i_state
     {
         assert(nullptr != m_owner);
 		m_owner->switch_state<t_crt, t_new_state>(m_slot_pos);
-        
+
 		//delete_state_impl<t_crt>::impl(m_slot_pos, m_owner);
 		//create_state_impl<t_new_state>::impl(m_slot_pos, m_owner);
     }
